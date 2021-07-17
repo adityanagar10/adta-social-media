90790efc424ee7c1a4c63c6ecfddf17edad72f72
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-07-17 21:36:56 +0530

    Routes added and Feed Complete

--- client/src/components/feed/feed.component.js
+++ client/src/components/feed/feed.component.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import {React , useState, useEffect} from 'react'
+import axios from "axios"
 import Post from "../post/post.component"
-import { Posts } from '../../dummyData'
 import Share from "../share/share.component"
 import './feed.styles.css'
 
 
 export default function Feed(){
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      const res = await axios.get("posts/timeline/60f2b041b6a7882d3c05c675");
+      setPosts(res.data)
+    }
+    fetchPosts()
+  },[])
     return (
         <div className="feed">
           <div className="feedWrapper">
            <Share />
-           {Posts.map((p) => (
-          <Post key={p.id} post={p} />
-        ))}
+            {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))} 
           </div>
         </div>
       );
--- client/src/components/post/post.component.js
+++ client/src/components/post/post.component.js
@@ -1,14 +1,24 @@
-
+import { useEffect, useState } from "react";
+import {Link} from "react-router-dom"
+import axios from "axios";
 import "./post.styles.css";
 import { MoreVert } from "@material-ui/icons";
-import { Users } from "../../dummyData";
-import { useState } from "react";
+import {format} from "timeago.js"
 
 
 export default function Post({ post }) {
   const [like,setLike] = useState(post.like)
+  const [user,setUser] = useState({})
   const [isLiked,setIsLiked] = useState(false)
   const pubfol = process.env.REACT_APP_PUBLIC_FOLDER
+
+  useEffect(()=>{
+    const fetchUser = async ()=>{
+      const res = await axios.get(`users/${post.userId}`);
+      setUser(res.data)
+    }
+    fetchUser()
+  },[post.userId])
 
   const likeHandler =()=>{
     setLike(isLiked ? like-1 : like+1)
@@ -19,15 +29,18 @@ export default function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
+            <Link to={`profile/${user.username}`}>
             <img
               className="postProfileImg"
-              src={Users.filter((u) => u.id === post?.userId)[0].profilePicture}
+              src={user.profilePicture || pubfol+"/person/noAvatar.png"}
               alt=""
             />
+
+            </Link>
             <span className="postUsername">
-              {Users.filter((u) => u.id === post?.userId)[0].username}
+              {user.username}
             </span>
-            <span className="postDate">{post.date}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -35,13 +48,13 @@ export default function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={pubfol+post.photo} alt="" />
+          <img className="postImg" src={pubfol+post.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon" src={pubfol+"/like.png"} onClick={likeHandler} alt="" />
             <img className="likeIcon" src={pubfol+"/heart.png"} onClick={likeHandler} alt="" />
-            <span className="postLikeCounter">{like} people like it</span>
+            <span className="postLikeCounter">{post.likes.length} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
