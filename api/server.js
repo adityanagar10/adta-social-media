7f338ea7226ad23b5240e35d3dcac854b644e3f2
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-07-19 19:15:33 +0530

    Miner Changes

--- api/server.js
+++ api/server.js
@@ -8,6 +8,9 @@ const app = express()
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const pathRoute = require("./routes/posts")
+const multer = require("multer")
+const path = require("path")
+
 
 dotenv.config()
 
@@ -21,11 +24,29 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 //middlewares
-
+app.use("/images", express.static(path.join(__dirname , "public/images")))
 app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(morgan('tiny'))
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null, "public/images")
+    },
+    filename: (req,file,cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage})
+app.post("/api/upload", upload.single("file"), (req,res)=>{
+    try{
+        return res.status(200).json("File uploaded")
+    }catch(err){
+        console.log(err)
+    }
+})
 
 
 //routes
